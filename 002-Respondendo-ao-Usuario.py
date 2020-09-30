a401760bdf605e23c096a249839af8dd7cc31b04
brcds <brunocristianocontato@gmail.com>
2020-09-29 23:01:51 -0300

    002/100

---
+++ 002-Respondendo-ao-Usuario.py
@@ -0,0 +1,5 @@
+#escreva que um programa que receba o nome do usuario e em seguida responda:
+#EX:É um prazer te conhecer "José"!
+
+nome = input('Digite seu nome: ')
+print(f'É um prazer te conhecer, {nome}')
