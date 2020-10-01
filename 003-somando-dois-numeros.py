2d58c534a74b58e761dc01412a2d5b5caa1890bd
brcds <brunocristianocontato@gmail.com>
2020-09-30 22:14:37 -0300

    003/100

---
+++ 003-somando-dois-numeros.py
@@ -0,0 +1,7 @@
+# Crie um programa que receba dois 2 numeros e apresente a soma deles.
+
+numero1 = int(input('Digite um valor: '))
+numero2 = int(input('Digite outro valor: '))
+soma = numero1 + numero2
+
+print(f'A soma entre {numero1} e {numero2} é igual a {soma}!')
