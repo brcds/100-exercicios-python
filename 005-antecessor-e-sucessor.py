646671aa2003e680483ab70c1dbdc9beeb3adf60
brcds <brunocristianocontato@gmail.com>
2020-10-02 23:02:31 -0300

    005/100

---
+++ 005-antecessor-e-sucessor.py
@@ -0,0 +1,7 @@
+#   Faça um programa que leia um numero inteiro e mostre na tela seu
+# sucessor e antecessor.
+
+numero = int(input("digite um numero: "))
+
+print(f'Analisando o valor {numero}, seu antecessor é {numero - 1} '
+      f'e o sucessor é {numero + 1}!')
