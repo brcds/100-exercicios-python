d701312ee468cbb4ae765211545b48da6e756aa9
brcds <brunocristianocontato@gmail.com>
2020-10-04 16:16:29 -0300

    007/100

---
+++ 007-media-aritmetica.py
@@ -0,0 +1,8 @@
+# Desenvolva um programa que leia duas notas de um aluno. Calcule e mostre sua média!
+
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+
+media = (nota1 + nota2) / 2
+
+print(f"\nA média entre {nota1:.1f} e {nota2:.1f} é igual a {media:.1f}")
