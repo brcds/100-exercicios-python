6445ace741a277659ab460b403f542e305ead5f7
brcds <brunocristianocontato@gmail.com>
2020-10-06 11:18:02 -0300

    009/100

--- 009-tabuada.py
+++ 009-tabuada.py
@@ -5,7 +5,7 @@ numero = int(input('Digite um número para ver sua tabuada: '))
 print('-' * 10)
 
 for c in range(1, 11):
-    print(f'{numero} * {c} = {numero * c}')
+    print(f'{numero} * {c:2} = {numero * c}')
 
 print('-' * 10)
 
