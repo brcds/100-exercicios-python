22e0075800320aaf744306cd9433ac78892da571
brcds <brunocristianocontato@gmail.com>
2020-10-12 03:01:27 -0300

    014/100

---
+++ 014-conversor-de-temperatura.py
@@ -0,0 +1,6 @@
+# escreva um programa que converta uma temperatura digitada em C° e converta para F°.
+
+C = float(input('Informe a temperatura em  °C: '))
+F = 9 * C / 5 + 32
+print(f'a temperatura de {C}°C corresponde a {F}°F!')
+
