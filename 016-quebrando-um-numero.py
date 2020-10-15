e44558f2410fde0b275777d9a46001cfa0fa295f
brcds <brunocristianocontato@gmail.com>
2020-10-14 23:04:50 -0300

    016/100

---
+++ 016-quebrando-um-numero.py
@@ -0,0 +1,5 @@
+#   Crie um programa que leia um numero real qualquer pelo teclado e mostre
+# sua porção inteira.
+
+numero = float(input("Digite um numero: "))
+print(f"O numero {numero} tem a parte inteira {int(numero)}.")
