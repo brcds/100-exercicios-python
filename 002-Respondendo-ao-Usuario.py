1bda1a35775e709c628e148d92a537768e60f364
Marcos Souza <mrcsz.m@gmail.com>
2020-10-18 15:26:33 -0300

    alternative solution

--- 002-Respondendo-ao-Usuario.py
+++ 002-Respondendo-ao-Usuario.py
@@ -1,5 +1,8 @@
 #escreva que um programa que receba o nome do usuario e em seguida responda:
 #EX:É um prazer te conhecer "José"!
 
-nome = input('Digite seu nome: ')
-print(f'É um prazer te conhecer, {nome}')
+def oi(nome = input('Digite seu nome: ')):
+	print(f'É um prazer te conhecer, {nome}')
+
+if __name__ == "__main__":
+	oi()
